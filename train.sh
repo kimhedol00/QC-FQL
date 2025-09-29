ee299bafab643b38f343cd87232133a4fe37f71f
Kim Juneseo <99635136+kimhedol00@users.noreply.github.com>
2025-09-29 13:17:45 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,16 @@
 export XLA_FLAGS=--xla_gpu_autotune_level=0
 export JAX_DEFAULT_matmul_precision='float32'
 export XLA_PYTHON_CLIENT_PREALLOCATE=false 
+export CUDA_VISIBLE_DEVICES=0
 
 MUJOCO_GL=egl python main.py \
-  --run_group=MY_ROBOT \
+  --eval_interval 0 \
+  --run_group=MY_ROBOT_HOR_10_MASKED \
   --env_name=AGI_TF \
   --sparse=True \
-  --horizon_length=10 \
-  --eval_interval 0 \
-  --custom_dataset_path ./final_robotics_dataset.h5 \
+  --horizon_length 10 \
+  --custom_dataset_dir ./preprocessed_datasets_state \
   --save_interval 10000 \
+  --log_interval 1000 \
   --online_steps 100000 \
-  --offline_steps 100000 \
+  --offline_steps 10000000 \
